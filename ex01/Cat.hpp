9ef20cf2d117fa65e1e6f6d34663d80ae1ac3458
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-11-29 22:27:17 +0900

    ex01:main:Animal array is double pointer and initialize the elements with cat and dog, and dog and cat have brain pointer. thisis kind of composition of classes. constructor allocs the pointer and destructor delete it, and copy-assignment operator delete original object pointer first and then copy the object of argment other.

---
+++ ex01/Cat.hpp
@@ -0,0 +1,19 @@
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Cat: public Animal
+{
+    private:
+        Brain *brain_;
+    public:
+        Cat(void);
+        Cat(Cat const &other);
+        Cat &operator=(Cat const &other);
+        ~Cat(void);
+        virtual void makeSound(void) const;
+};
+
+#endif
---
+++ ex01/Dog.hpp
@@ -0,0 +1,19 @@
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Dog : public Animal
+{
+    private:
+        Brain *brain_;
+    public:
+        Dog(void);
+        Dog(Dog const &other);
+        Dog const &operator=(Dog const &other);
+        ~Dog(void);
+        virtual void makeSound(void) const;
+};
+
+#endif
