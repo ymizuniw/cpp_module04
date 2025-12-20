645584a719d4a939f6af416d248e85457913416d
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-12-21 01:19:16 +0900

    all:Makefile:fclean, re dependency fixed. Test made rapid

--- whole_test.sh
+++ whole_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-VALG=("valgrind" "--leak-check=full" "--show-leak-kinds=all" "--error-exitcode=1" "-q")
+# VALG=("valgrind" "--leak-check=full" "--show-leak-kinds=all" "--error-exitcode=1" "-q")
+VALG=
+MAKE_FAST=("make" "-j$(nproc)")
 
-MAKE_FAST=("make" "re" "-j$(nproc)")
-
-cd ex00 && ${MAKE_FAST[@]} -s && ${VALG[@]} ./test00
-${MAKE_FAST[@]} -s "DEBUG=1" && ${VALG[@]} ./test00
+cd ex00 && ${MAKE_FAST[@]} re -s && ${VALG[@]} ./test00
+${MAKE_FAST[@]} re -s "DEBUG=1" && ${VALG[@]} ./test00
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
     printf "%s\n" "ex00:ERROR!"
@@ -13,20 +13,20 @@ if [ $EXIT_STATUS -ne 0 ]; then
 fi
 
 cd ..
-cd ex01 && ${MAKE_FAST[@]} -s && ${VALG[@]} ./test00
-${MAKE_FAST[@]} -s "DEBUG=1" && ${VALG[@]} ./test00
+cd ex01 && ${MAKE_FAST[@]} re -s && ${VALG[@]} ./test00
+${MAKE_FAST[@]} re -s "DEBUG=1" && ${VALG[@]} ./test00
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
     printf "%s\n" "ex01:ERROR!"
     exit 1
 fi
-${MAKE_FAST[@]} -s && ${VALG[@]} ./test01
+${MAKE_FAST[@]} re -s && ${VALG[@]} ./test01
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
     printf "%s\n" "ex01:ERROR!"
     exit 1
 fi
-${MAKE_FAST[@]} -s "DEBUG=1" && ${VALG[@]} ./test01
+${MAKE_FAST[@]} re -s "DEBUG=1" && ${VALG[@]} ./test01
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
     printf "%s\n" "ex01:ERROR!"
@@ -34,20 +34,20 @@ if [ $EXIT_STATUS -ne 0 ]; then
 fi
 
 cd ..
-cd ex02 && ${MAKE_FAST[@]} -s && ${VALG[@]} ./test00
-${MAKE_FAST[@]} -s "DEBUG=1" && ${VALG[@]} ./test00
+cd ex02 && ${MAKE_FAST[@]} re -s && ${VALG[@]} ./test00
+${MAKE_FAST[@]} re -s "DEBUG=1" && ${VALG[@]} ./test00
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
     printf "%s\n" "ex02:ERROR!"
     exit 1
 fi
-${MAKE_FAST[@]} -s && ${VALG[@]} ./test01
+${MAKE_FAST[@]} re -s && ${VALG[@]} ./test01
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
     printf "%s\n" "ex02:ERROR!"
     exit 1
 fi
-${MAKE_FAST[@]} -s "DEBUG=1" && ${VALG[@]} ./test01
+${MAKE_FAST[@]} re -s "DEBUG=1" && ${VALG[@]} ./test01
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
     printf "%s\n" "ex02:ERROR!"
@@ -55,10 +55,10 @@ if [ $EXIT_STATUS -ne 0 ]; then
 fi
 
 printf "%s\n" "All Tests Passed!"
-
-cd ex00 && ${MAKE_FAST[@]} fclean
 cd ..
-cd ex01 && ${MAKE_FAST[@]} fclean
+cd ex00 && ${MAKE_FAST[@]} -s fclean
 cd ..
-cd ex02 && ${MAKE_FAST[@]} fclean
+cd ex01 && ${MAKE_FAST[@]} -s fclean
+cd ..
+cd ex02 && ${MAKE_FAST[@]} -s fclean
 cd ..
